0ec2486c10d02e62107d79fe73073998416cc472
Wyatt Johnson <wyatt.j1834@gmail.com>
2025-11-15 21:46:15 -0700

    fix env check for gpio

--- src/models/relay.js
+++ src/models/relay.js
@@ -1,7 +1,7 @@
 import { Gpio } from 'onoff';
 export default class Relay {
     constructor({ id = null, name, gpio_pin, active = 0 }) {
-        const gpioEnabled = process.env.GPIO_ENABLED == true;
+        const gpioEnabled = process.env.GPIO_ENABLED == 'true';
         this.id = id;
         this.name = name;
         this.gpio_pin = gpio_pin;
--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ import express from 'express';
 import routes from './routes/api.js';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
